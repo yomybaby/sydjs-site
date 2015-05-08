d162751d41706bc7b6c44da207e99900436b0c48
Jong Eun Lee <yomybaby@yomybaby.com>
2015-05-08 15:53:12 +0900

    add talk properties and update date format

--- models/Talk.js
+++ models/Talk.js
@@ -20,14 +20,16 @@ Talk.add({
 	who: { type: Types.Relationship, ref: 'User', many: true, index: true },
 	description: { type: Types.Html, wysiwyg: true },
 	slides: { type: Types.Url },
-	link: { type: Types.Url }
+	link: { type: Types.Url },
+	github: { type: Types.Url },
+	video: { type: Types.Url }
 });
 
 Talk.schema.set('toJSON', {
 	virtuals: true,
 	transform: function(doc, rtn, options) {
 		
-		rtn = _.pick(rtn, '_id', 'name', 'place', 'map', 'description', 'slides', 'link');
+		rtn = _.pick(rtn, '_id', 'name', 'place', 'map', 'description', 'slides', 'link', 'github', 'video');
 		
 		if (doc.who) {
 			rtn.who = doc.who.map(function(i) {
